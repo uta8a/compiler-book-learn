ce35b5c4d2cccc83ec7e33f9c8c7a504b4fc472c
kaito_tateyama <noumodulity@gmail.com>
2019-07-24 14:12:28 +0900

    UPDATE: ND_ADD only implement

--- 9cc.c
+++ 9cc.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+// ----------token------------
 typedef enum {
 	TK_RESERVED, // symbol
 	TK_NUM,		 // number token 
@@ -23,6 +24,28 @@ struct Token {
 
 // now focused token
 Token *token;
+
+// ----------Node--------------
+typedef enum {
+	ND_ADD, // +
+	ND_SUB, // -
+	ND_MUL, // *
+	ND_DIV, // /
+	ND_NUM, // number
+} NodeKind;
+
+typedef struct Node Node;
+
+struct Node {
+	NodeKind kind;
+	Node *lhs;
+	Node *rhs;
+	int val;
+};
+
+Node *expr();
+Node *mul();
+Node *term();
 
 // error report function
 // like printf
@@ -109,6 +132,69 @@ Token *tokenize(char *p) {
 	return head.next;
 }
 
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+	Node *node = calloc(1,sizeof(Node));
+	node->kind = kind;
+	node->lhs = lhs;
+	node->rhs = rhs;
+	return node;
+}
+
+Node *new_node_num(int val) {
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = ND_NUM;
+	node->val = val;
+	return node;
+}
+
+Node *expr() {
+	Node *node = mul();
+	
+	for(;;) {
+		if (consume('+'))
+			node =  new_node(ND_ADD, node, mul());
+		else if (consume('-'))
+			node = new_node(ND_SUB, node, mul());
+		else
+			return node;
+	}
+}
+
+Node *mul() {
+	Node *node = term();
+
+	return node;
+}
+Node *term() {
+	if (consume('(')) {
+		Node *node = expr();
+		expect(')');
+		return node;
+	}
+
+	return new_node_num(expect_number());
+}
+
+void gen(Node *node) {
+	if (node->kind == ND_NUM) {
+		printf("  push %d\n", node->val);
+		return;
+	}
+
+	gen(node->lhs);
+	gen(node->rhs);
+
+	printf("  pop rdi\n");
+	printf("  pop rax\n");
+
+	switch (node->kind) {
+	case ND_ADD:
+		printf("  add rax, rdi\n");
+		break;
+	}
+	printf("  push rax\n");
+}
+
 int main(int argc, char **argv) {
 	if (argc != 2) {
 		fprintf(stderr, "number of argument is not true.\n");
@@ -116,29 +202,18 @@ int main(int argc, char **argv) {
 	}
 
 	// tokenize
-	token = tokenize(argv[1]);
 	user_input = argv[1];
+	token = tokenize(user_input);
+	Node *node = expr();
 
 	// assembly front part
 	printf(".intel_syntax noprefix\n");
 	printf(".global main\n");
 	printf("main:\n");
 
-	// formula head must be number
-	// mov
-	printf("  mov rax, %d\n", expect_number());
+	gen(node);
 
-	// use `+ num` or `- num` pattern
-	while(!at_eof()) {
-		if (consume('+')) {
-			printf("  add rax, %d\n", expect_number());
-			continue;
-		}
-
-		expect('-');
-		printf("  sub rax, %d\n", expect_number());
-	}
-
+	printf("  pop rax\n");
 	printf("  ret\n");
 	return 0;
 }
